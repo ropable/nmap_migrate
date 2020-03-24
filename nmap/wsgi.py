a3264a1576c4d824f48adfe33cacf49b47285818
Ashley Felton <ashley@ropable.com>
2020-03-24 13:26:54 +0800

    Update wsgi.py to optionally read .env file.

--- nmap/wsgi.py
+++ nmap/wsgi.py
@@ -1,10 +1,12 @@
 from django.core.wsgi import get_wsgi_application
 import dotenv
 import os
+from pathlib2 import Path
 
-dot_env = os.path.join(os.getcwd(), '.env')
+d = Path(__file__).resolve().parents[1]
+dot_env = os.path.join(str(d), '.env')
 if os.path.exists(dot_env):
-    dotenv.read_dotenv()
+    dotenv.read_dotenv(dot_env)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nmap.settings")
 application = get_wsgi_application()
